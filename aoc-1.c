93813157d50900985164f57b3c1e6585c46ebb80
Jean Ernani Witt Meier <71325720+jeanwm@users.noreply.github.com>
2023-08-11 19:54:24 -0300

    Add files via upload

--- aoc-1.c
+++ aoc-1.c
@@ -3,10 +3,20 @@
 
 
 int main (int argc, const char *argv[]) {
-	int a, b, i;
+	int a, b, c, i;
 	
 	a = 0b101;
 	b = 0b010;
+	c = 0b100;
+	
+	// c = c >> 1;
+	// c passa a ser 010;
+	
+	// c = c >> 2;
+	// c passa a ser 001;
+	
+	// operadores bituais
+	// &, |, ~
 	
 	// %X para escrever hexadecimal
 	printf("%X", a | b);
